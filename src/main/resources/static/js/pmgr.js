c7745fd8fce52623973ceb1627d33f77aaadb4da
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-12-04 16:14:37 +0100

    cambio credenciales

--- src/main/resources/static/js/pmgr.js
+++ src/main/resources/static/js/pmgr.js
@@ -453,10 +453,10 @@ const login = (username, password) => {
         });
 }
 
-                 // -- IMPORTANTE --
-login("p", "p"); // <-- tu nombre de usuario y password aquí
-                 //   y puedes re-logearte como alguien distinto desde  la consola
-                 //   llamando a login() con otro usuario y contraseña
+// -- IMPORTANTE --
+login("g7", "8RKrb"); // <-- tu nombre de usuario y password aquí
+//   y puedes re-logearte como alguien distinto desde  la consola
+//   llamando a login() con otro usuario y contraseña
 {
     /** 
      * Asocia comportamientos al formulario de añadir películas 
